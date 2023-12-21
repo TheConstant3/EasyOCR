6c94cce3c6b1bd749e85a057b958d59afd189c73
Konstantin <46416203+TheConstant3@users.noreply.github.com>
2023-12-21 15:57:22 +0300

    Update vgg_model.py

--- easyocr/model/vgg_model.py
+++ easyocr/model/vgg_model.py
@@ -1,6 +1,12 @@
 import torch.nn as nn
 from .modules import VGG_FeatureExtractor, BidirectionalLSTM
 
+
+class AvgLayer(nn.Module):
+     def forward(self, input):
+        return input.mean(3, keepdim=True)
+
+
 class Model(nn.Module):
 
     def __init__(self, input_channel, output_channel, hidden_size, num_class):
@@ -8,7 +14,8 @@ class Model(nn.Module):
         """ FeatureExtraction """
         self.FeatureExtraction = VGG_FeatureExtractor(input_channel, output_channel)
         self.FeatureExtraction_output = output_channel
-        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))
+        # self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))
+        self.AdaptiveAvgPool = AvgLayer()
 
         """ Sequence modeling"""
         self.SequenceModeling = nn.Sequential(
@@ -20,7 +27,7 @@ class Model(nn.Module):
         self.Prediction = nn.Linear(self.SequenceModeling_output, num_class)
 
 
-    def forward(self, input, text):
+    def forward(self, input):
         """ Feature extraction stage """
         visual_feature = self.FeatureExtraction(input)
         visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 3, 1, 2))
