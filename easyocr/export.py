5da311a384c3e9c7b15f6e874bb03234f876d588
nikshorop <nikshorop@gmail.com>
2023-12-21 16:17:27 +0300

    add export recognizer

--- easyocr/export.py
+++ easyocr/export.py
@@ -7,14 +7,15 @@ import numpy as np
 
 
 def export_detector(detector_onnx_save_path,
-                    in_shape=[1, 3, 608, 800],
+                    recognizer_onnx_save_path,
+                    in_det_shape=[1, 3, 608, 800],
+                    in_rec_shape=[1, 1, 64, 512],
                     lang_list=["en"],
                     model_storage_directory=None,
                     user_network_directory=None,
                     download_enabled=True,
                     dynamic=True,
                     device="cpu",
-                    quantize=True,
                     detector=True,
                     recognizer=True):
     if dynamic is False:
@@ -22,15 +23,15 @@ def export_detector(detector_onnx_save_path,
     ocr_reader = easyocr.Reader(lang_list,
                                 gpu=False if device == "cpu" else True,
                                 detector=detector,
-                                recognizer=detector,
-                                quantize=quantize,
+                                recognizer=recognizer,
+                                quantize=False,
                                 model_storage_directory=model_storage_directory,
                                 user_network_directory=user_network_directory,
                                 download_enabled=download_enabled)
 
     # exporting detector if selected
     if detector:
-        dummy_input = torch.rand(in_shape)
+        dummy_input = torch.rand(in_det_shape)
         dummy_input = dummy_input.to(device)
 
         # forward pass
@@ -83,23 +84,86 @@ def export_detector(detector_onnx_save_path,
         print(f"Model exported to {detector_onnx_save_path} and tested with ONNXRuntime, and the result looks good!")
 
 
+    # exporting detector if selected
+    if recognizer:
+        dummy_input = torch.rand(in_rec_shape)
+        dummy_input = dummy_input.to(device)
+
+        # forward pass
+        with torch.no_grad():
+            y_torch_out, feature_torch_out = ocr_reader.recognizer(dummy_input)
+            torch.onnx.export(ocr_reader.recognizer,
+                              dummy_input,
+                              recognizer_onnx_save_path,
+                              export_params=True,
+                              do_constant_folding=True,
+                              opset_version=12,
+                              # model's input names
+                              input_names=['input'],
+                              # model's output names, ignore the 2nd output
+                              output_names=['output'],
+                              # variable length axes
+                              dynamic_axes={'input': {0: 'batch_size', 3: "width"},
+                                            'output': {0: 'batch_size', 2: "dim2"}
+                                            } if dynamic else None,
+                              verbose=False)
+
+        # verify exported onnx model
+        recognizer_onnx = onnx.load(recognizer_onnx_save_path)
+        onnx.checker.check_model(recognizer_onnx)
+        print(f"Model Inputs:\n {recognizer_onnx.graph.input}\n{'*'*80}")
+        print(f"Model Outputs:\n {recognizer_onnx.graph.output}\n{'*'*80}")
+
+        # onnx inference validation
+        import onnxruntime
+
+        ort_session = onnxruntime.InferenceSession(recognizer_onnx_save_path)
+
+        def to_numpy(tensor):
+            if tensor.requires_grad:
+                return tensor.detach().cpu().numpy()
+            return tensor.cpu().numpy()
+
+        # compute ONNX Runtime output prediction
+        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(dummy_input)}
+        y_onnx_out, feature_onnx_out = ort_session.run(None, ort_inputs)
+
+        print(f"torch outputs: y_torch_out.shape={y_torch_out.shape} feature_torch_out.shape={feature_torch_out.shape}")
+        print(f"onnx outputs: y_onnx_out.shape={y_onnx_out.shape} feature_onnx_out.shape={feature_onnx_out.shape}")
+        # compare ONNX Runtime and PyTorch results
+        np.testing.assert_allclose(
+            to_numpy(y_torch_out), y_onnx_out, rtol=1e-03, atol=1e-05)
+        np.testing.assert_allclose(
+            to_numpy(feature_torch_out), feature_onnx_out, rtol=1e-03, atol=1e-05)
+
+        print(f"Model exported to {recognizer_onnx_save_path} and tested with ONNXRuntime, and the result looks good!")
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--lang_list',
                         nargs='+', type=str,
                         default=["en"],
                         help='-l en ch_sim ... (language lists for easyocr)')
-    parser.add_argument('-s', '--detector_onnx_save_path', type=str,
+    parser.add_argument('-dp', '--detector_onnx_save_path', type=str,
                         default="detector_craft.onnx",
-                        help="export detector onnx file path ending in .onnx" +
+                        help="export detector onnx file path ending in .onnx" + 
+                        "Do not pass in this flag to avoid exporting detector")
+    parser.add_argument('-rp', '--recognizer_onnx_save_path', type=str,
+                        default="recognizer_g2.onnx",
+                        help="export detector onnx file path ending in .onnx" + 
                         "Do not pass in this flag to avoid exporting detector")
     parser.add_argument('-d', '--dynamic',
                         action='store_true',
                         help="Dynamic  input output shapes for detector")
-    parser.add_argument('-is', '--in_shape',
+    parser.add_argument('-ids', '--in_det_shape',
                         nargs='+', type=int,
                         default=[1, 3, 608, 800],
                         help='-is 1 3 608 800 (bsize, channel, height, width)')
+    parser.add_argument('-irs', '--in_rec_shape',
+                        nargs='+', type=int,
+                        default=[1, 1, 64, 512],
+                        help='-is 1 1 64 512 (bsize, channel, height, width)')
     parser.add_argument('-m', '--model_storage_directory', type=str,
                         help="model storage directory for craft model")
     parser.add_argument('-u', '--user_network_directory', type=str,
@@ -116,7 +180,9 @@ def parse_args():
 def main():
     args = parse_args()
     export_detector(detector_onnx_save_path=args.detector_onnx_save_path,
-                    in_shape=args.in_shape,
+                    recognizer_onnx_save_path=args.recognizer_onnx_save_path,
+                    in_det_shape=args.in_det_shape,
+                    in_rec_shape=args.in_rec_shape,
                     lang_list=args.lang_list,
                     model_storage_directory=args.model_storage_directory,
                     user_network_directory=args.user_network_directory,
