a9a89521dada7f413ac43f591931bc21788a9b05
nikshorop <nikshorop@gmail.com>
2023-12-21 17:02:35 +0300

    fix rec export

--- easyocr/export.py
+++ easyocr/export.py
@@ -91,7 +91,7 @@ def export_detector(detector_onnx_save_path,
 
         # forward pass
         with torch.no_grad():
-            y_torch_out, feature_torch_out = ocr_reader.recognizer(dummy_input)
+            y_torch_out = ocr_reader.recognizer(dummy_input)
             torch.onnx.export(ocr_reader.recognizer,
                               dummy_input,
                               recognizer_onnx_save_path,
@@ -128,13 +128,13 @@ def export_detector(detector_onnx_save_path,
         ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(dummy_input)}
         y_onnx_out, feature_onnx_out = ort_session.run(None, ort_inputs)
 
-        print(f"torch outputs: y_torch_out.shape={y_torch_out.shape} feature_torch_out.shape={feature_torch_out.shape}")
-        print(f"onnx outputs: y_onnx_out.shape={y_onnx_out.shape} feature_onnx_out.shape={feature_onnx_out.shape}")
+        print(f"torch outputs: y_torch_out.shape={y_torch_out.shape}")
+        print(f"onnx outputs: y_onnx_out.shape={y_onnx_out.shape}")
         # compare ONNX Runtime and PyTorch results
         np.testing.assert_allclose(
             to_numpy(y_torch_out), y_onnx_out, rtol=1e-03, atol=1e-05)
-        np.testing.assert_allclose(
-            to_numpy(feature_torch_out), feature_onnx_out, rtol=1e-03, atol=1e-05)
+        # np.testing.assert_allclose(
+        #     to_numpy(feature_torch_out), feature_onnx_out, rtol=1e-03, atol=1e-05)
 
         print(f"Model exported to {recognizer_onnx_save_path} and tested with ONNXRuntime, and the result looks good!")
 
